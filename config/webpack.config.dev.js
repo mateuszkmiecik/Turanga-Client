c2f7b8d5d563eda83df2e5bdd5d1566bee5fe8b9
Mateusz Kmiecik <mk.chomik@gmail.com>
2017-01-04 20:12:12 +0100

    Added react-quill

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -83,6 +83,10 @@ module.exports = {
     },
 
     module: {
+        noParse: [
+            path.resolve('node_modules/react-quill/node_modules/quill/dist/quill.js'), //npm 2
+            path.resolve('node_modules/quill/dist/quill.js') //npm 3
+        ],
         // First, run the linter.
         // It's important to do this before Babel processes the JS.
         preLoaders: [
