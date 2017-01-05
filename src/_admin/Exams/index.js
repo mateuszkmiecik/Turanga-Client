af62b5f4da4157f386fed4eec9adcca298ac3a45
Mateusz Kmiecik <mk.chomik@gmail.com>
2017-01-05 05:04:00 +0100

    Proper exams boilerplate

--- src/_admin/Exams/index.js
+++ src/_admin/Exams/index.js
@@ -1,1 +1,13 @@
-rcls
+import React, {
+    Component,
+} from 'react';
+
+class Exams extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Exams;
