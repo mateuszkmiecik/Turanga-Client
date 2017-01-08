2b914cbe1ab8bfa430982cfe9b5a5b5531b0a6ec
Mateusz Kmiecik <mk.chomik@gmail.com>
2017-01-08 13:02:41 +0100

    Results showing from backend

--- src/_user/Results.js
+++ src/_user/Results.js
@@ -1,9 +1,10 @@
 import React, {
     Component,
 } from 'react';
+import {Tab, TabList, TabPanel, Tabs, Alert} from "@blueprintjs/core";
+
 
 import API from '../services/API'
-
 import {Content} from '../components'
 
 
@@ -13,104 +14,92 @@ class Results extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            results: []
+            exams: [],
+            categories: []
         }
     }
 
     componentDidMount() {
 
-        API.get('/student/attempts').then(attempts => console.log(attempts))
+        API.get('/student/attempts/ex?isFinished=true').then(exams => this.setState({
+            exams
+        }));
+        API.get('/student/attempts/cat?isFinished=true').then(categories => this.setState({
+            categories
+        }));
 
     }
 
 
     render() {
+        const {exams, categories} = this.state;
         return (
             <Content col="10">
                 <div className="panel results full-height">
                     <div className="panel-body">
                         <h3>Results</h3>
 
-                        <div className="pt-card pt-elevation-0 pt-interactive space-bottom"
-                             onClick={() => this.props.router.push('/results/1')}>
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <strong>User</strong>
-                                    <p>User 1</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Date</strong>
-                                    <p>{new Date().toDateString()}</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Result</strong>
-                                    <p>4/10</p>
-                                    <div className="pt-progress-bar pt-intent-primary pt-no-animation">
-                                        <div className="pt-progress-meter" style={{"width": '40%'}}></div>
+                        <Tabs>
+                            <TabList className="pt-large">
+                                <Tab>Categories</Tab>
+                                <Tab>Exams</Tab>
+                            </TabList>
+                            <TabPanel>
+                                <div className="row">
+                                    <div className="col-sm-7">
+                                        {categories.map((catAttempt, i) => (
+                                            <div className="pt-card pt-elevation-0 pt-interactive space-bottom" key={i}>
+                                                <div className="row">
+                                                    <div className="col-sm-6">
+                                                        <strong>Category name</strong>
+                                                        <p>{catAttempt.name}</p>
+                                                    </div>
+                                                    <div className="col-sm-3">
+                                                        <strong>Date</strong>
+                                                        <p>{new Date().toDateString()}</p>
+                                                    </div>
+                                                    <div className="col-sm-3">
+                                                        <strong>Score</strong>
+                                                        <p>{catAttempt.score}/{catAttempt.tasks.length}</p>
+                                                        <div className="pt-progress-bar pt-intent-primary pt-no-animation">
+                                                            <div className="pt-progress-meter" style={{"width": "50%"}}></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="pt-card pt-elevation-0 pt-interactive space-bottom"
-                             onClick={() => this.props.router.push('/results/2')}>
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <strong>User</strong>
-                                    <p>User 2</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Date</strong>
-                                    <p>{new Date().toDateString()}</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Result</strong>
-                                    <p>7/10</p>
-                                    <div className="pt-progress-bar pt-intent-primary pt-no-animation">
-                                        <div className="pt-progress-meter" style={{"width": '70%'}}></div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div className="row">
+                                    <div className="col-sm-7">
+                                        {exams.map((examAttempt, i) => (
+                                            <div className="pt-card pt-elevation-0 pt-interactive space-bottom" key={i}>
+                                                <div className="row">
+                                                    <div className="col-sm-6">
+                                                        <strong>Exam name</strong>
+                                                        <p>{examAttempt.name}</p>
+                                                    </div>
+                                                    <div className="col-sm-3">
+                                                        <strong>Date</strong>
+                                                        <p>{new Date().toDateString()}</p>
+                                                    </div>
+                                                    <div className="col-sm-3">
+                                                        <strong>Score</strong>
+                                                        <p>{examAttempt.score}/{examAttempt.tasks.length}</p>
+                                                        <div className="pt-progress-bar pt-intent-primary pt-no-animation">
+                                                            <div className="pt-progress-meter" style={{"width": `${100*examAttempt.score/examAttempt.tasks.length}%`}}></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="pt-card pt-elevation-0 pt-interactive space-bottom"
-                             onClick={() => this.props.router.push('/results/3')}>
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <strong>User</strong>
-                                    <p>User 3</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Date</strong>
-                                    <p>{new Date().toDateString()}</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Result</strong>
-                                    <p>10/10</p>
-                                    <div className="pt-progress-bar pt-intent-success pt-no-animation">
-                                        <div className="pt-progress-meter" style={{"width": '100%'}}></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="pt-card pt-elevation-0 pt-interactive space-bottom"
-                             onClick={() => this.props.router.push('/results/4')}>
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <strong>User</strong>
-                                    <p>User 4</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Date</strong>
-                                    <p>{new Date().toDateString()}</p>
-                                </div>
-                                <div className="col-sm-3">
-                                    <strong>Result</strong>
-                                    <p>3/10</p>
-                                    <div className="pt-progress-bar pt-intent-danger pt-no-animation">
-                                        <div className="pt-progress-meter" style={{"width": '30%'}}></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
+                            </TabPanel>
+                        </Tabs>
                     </div>
                 </div>
             </Content>
