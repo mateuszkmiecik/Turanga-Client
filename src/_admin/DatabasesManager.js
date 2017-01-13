45e589003478421712e52216ef3ee0096ec02236
Mateusz Kmiecik <mk.chomik@gmail.com>
2017-01-13 18:24:18 +0100

    Editing databases

--- src/_admin/DatabasesManager.js
+++ src/_admin/DatabasesManager.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import Dropzone from 'react-dropzone'
 
+import {Intent, Dialog, Button} from '@blueprintjs/core'
+
 import Select from 'react-select'
 import {Content, Sidebar} from '../components'
 import Databases from '../services/Databases'
@@ -22,7 +24,16 @@ class DatabasesManager extends Component {
                 dbEngine: '',
                 files: []
             },
-            dbCodes: []
+            editDatabase: {
+                name: '',
+                url: '',
+                user: '',
+                password: '',
+                dbEngine: '',
+                files: []
+            },
+            dbCodes: [],
+            editDialogOpen: false
         };
 
 
@@ -43,7 +54,7 @@ class DatabasesManager extends Component {
 
     refresh() {
         Databases.get().then(databases => this.setState({
-            databases
+            databases: databases.map(obj => ({...obj, dbEngine: {code: obj.dbEngine}}))
         }))
     }
 
@@ -82,6 +93,33 @@ class DatabasesManager extends Component {
 
     }
 
+    updateDatabase(db){
+
+        let promise = Promise.resolve({});
+
+        if (db.files && db.files.length > 0) {
+            promise = API.upload(db.files)
+        }
+
+        promise.then(({filename}) => {
+            let {_id, name, url, user, password, dbEngine} = db;
+            let newObj = {
+                name, url, user, password, dbEngine
+            };
+            newObj.dbEngine = newObj.dbEngine.code;
+            if (filename) {
+                newObj.schemeFile = filename;
+            }
+            return Databases.updateDB(_id, newObj)
+        }).then(() => {
+            this.refresh();
+            this.setState({
+                editDialogOpen: false
+            })
+        });
+
+    }
+
     onInputChange(objectName, fieldName) {
         return e =>
             this.setState({
@@ -93,11 +131,10 @@ class DatabasesManager extends Component {
     }
 
     changeDBEngine(objectName, field, value) {
-        console.log(value);
         this.setState({
             [objectName]: {
                 ...this.state[objectName],
-                [field] : value
+                [field]: value
             }
         })
     }
@@ -108,7 +145,7 @@ class DatabasesManager extends Component {
 
     render() {
 
-        let {databases, newDatabase} = this.state;
+        let {databases, newDatabase, editDatabase} = this.state;
         const allCorrect = ['name', 'url', 'user', 'password', 'dbEngine'].every(field => (newDatabase[field].length > 0 || !!newDatabase[field]));
 
         return (
@@ -124,45 +161,53 @@ class DatabasesManager extends Component {
                                 <hr/>
 
                                 {databases.length > 0 ? (
-                                    <table className="table table-bordered">
-                                        <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Name</th>
-                                            <th>Url</th>
-                                            <th>User</th>
-                                            <th>Pass</th>
-                                            <th>DB Engine</th>
-                                            <th>Scheme file</th>
-                                            <th/>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        {databases.map((db, idx) => (
-                                            <tr key={db._id}>
-                                                <td>{idx + 1}</td>
-                                                <td>{db.name}</td>
-                                                <td>{db.url}</td>
-                                                <td>{db.user}</td>
-                                                <td>{db.password}</td>
-                                                <td>{db.dbEngine}</td>
-                                                <td><a href={`http://localhost:8080/static/${db.schemeFile}`}
-                                                       target="_blank">{db.schemeFile}</a></td>
-                                                <td>
-                                                    <button className="btn btn-xs"
-                                                            onClick={() => this.removeDB(db)}>
-                                                        <i className="fa fa-trash"/>
-                                                    </button>
-                                                </td>
+                                        <table className="table table-bordered">
+                                            <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Name</th>
+                                                <th>Url</th>
+                                                <th>User</th>
+                                                <th>Pass</th>
+                                                <th>DB Engine</th>
+                                                <th>Scheme file</th>
+                                                <th/>
                                             </tr>
-                                        ))}
-                                        </tbody>
-                                    </table>
-                                ) : (
-                                    <div className="panel-body">
-                                        No databases added yet.
-                                    </div>
-                                )}
+                                            </thead>
+                                            <tbody>
+                                            {databases.map((db, idx) => (
+                                                <tr key={db._id}>
+                                                    <td>{idx + 1}</td>
+                                                    <td>{db.name}</td>
+                                                    <td>{db.url}</td>
+                                                    <td>{db.user}</td>
+                                                    <td>{db.password}</td>
+                                                    <td>{(db.dbEngine || {}).code}</td>
+                                                    <td><a href={`http://localhost:8080/static/${db.schemeFile}`}
+                                                           target="_blank">{db.schemeFile}</a></td>
+                                                    <td>
+
+                                                        <button className="btn btn-xs space-right"
+                                                                onClick={() => this.setState({
+                                                                    editDatabase: db,
+                                                                    editDialogOpen: true
+                                                                })}>
+                                                            <i className="fa fa-edit"/>
+                                                        </button>
+                                                        <button className="btn btn-xs"
+                                                                onClick={() => this.removeDB(db)}>
+                                                            <i className="fa fa-trash"/>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                            </tbody>
+                                        </table>
+                                    ) : (
+                                        <div className="panel-body">
+                                            No databases added yet.
+                                        </div>
+                                    )}
                             </div>
 
 
@@ -174,7 +219,7 @@ class DatabasesManager extends Component {
                                 <p>
                                     <label>Name</label>
                                     <input type="text" className="pt-input pt-fill" placeholder="alias"
-                                           value={newDatabase.name}
+                                           value={editDatabase.name}
                                            onChange={this.onInputChange('newDatabase', 'name')}/>
                                 </p>
 
@@ -227,6 +272,78 @@ class DatabasesManager extends Component {
                         </div>
                     </div>
                 </div>
+
+                <Dialog isOpen={this.state.editDialogOpen} onClose={() => this.setState({
+                    editDialogOpen: false
+                })}>
+                    <div className="pt-dialog-body">
+                        <p>
+                            <label>Name</label>
+                            <input type="text" className="pt-input pt-fill" placeholder="alias"
+                                   value={editDatabase.name}
+                                   onChange={this.onInputChange('editDatabase', 'name')}/>
+                        </p>
+
+                        <p>
+                            <label>Server URL</label>
+                            <input type="text" className="pt-input pt-fill" placeholder="url"
+                                   value={editDatabase.url}
+                                   onChange={this.onInputChange('editDatabase', 'url')}/>
+                        </p>
+                        <p>
+                            <label>User</label>
+                            <input type="text" className="pt-input pt-fill" placeholder="user"
+                                   value={editDatabase.user}
+                                   onChange={this.onInputChange('editDatabase', 'user')}/>
+                        </p>
+                        <p>
+                            <label>Password</label>
+                            <input type="text" className="pt-input pt-fill" placeholder="password"
+                                   value={editDatabase.password}
+                                   onChange={this.onInputChange('editDatabase', 'password')}/>
+                        </p>
+                        <p>
+                            <label>DB Engine</label>
+                            {JSON.stringify(editDatabase.dbEngine)}
+                            <Select multi={false} value={editDatabase.dbEngine} labelKey={"code"}
+                                    onChange={dbCode => this.changeDBEngine('editDatabase', 'dbEngine', dbCode)}
+                                    options={this.state.dbCodes}/>
+                        </p>
+                        <div>
+                            <label>Schema file</label>
+                            <Dropzone onDrop={(acceptedFiles, rejectedFiles) => this.setState({
+                                editDatabase: {
+                                    ...editDatabase,
+                                    files: [...acceptedFiles]
+                                }
+                            })} className="pt-callout space-bottom pt-intent-warning"
+                                      style={{textAlign: 'center'}}>
+                                {(editDatabase.files || []).length > 0 ?
+                                    editDatabase.files.map(f => f.name) :
+                                    <div>Try dropping some files here, or click to select files to
+                                        upload.</div> }
+                            </Dropzone>
+                        </div>
+                        <p>
+                            <button className="pt-button pt-intent-primary"
+                                    disabled={!allCorrect}
+                                    onClick={() => this.updateDatabase(editDatabase)}>Save
+                            </button>
+                        </p>
+                    </div>
+                    <div className="pt-dialog-footer">
+                        <div className="pt-dialog-footer-actions">
+                            <Button text="Cancel" onClick={() => this.setState({
+                                editDialogOpen: false
+                            })}/>
+                            <Button
+                                intent={Intent.PRIMARY}
+                                onClick={() => this.updateDatabase(editDatabase)}
+                                text="Save"
+                            />
+                        </div>
+                    </div>
+                </Dialog>
 
             </Content>
         )
--- src/services/Databases.js
+++ src/services/Databases.js
@@ -3,6 +3,7 @@ import API from './API'
 
 const get = () => API.get(`/databases`);
 const createDB = (db) => API.post(`/databases`, db);
+const updateDB = (id, db) => API.put(`/databases/${id}`, db);
 const deleteDB = (db) => API.delete(`/databases/${db._id}`)
 
-export default {get, createDB, deleteDB}
+export default {get, createDB, updateDB, deleteDB}
